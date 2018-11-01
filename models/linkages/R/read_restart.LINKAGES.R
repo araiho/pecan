4f8337e9768ef4ba2954e357550888773419fdf6
araiho <ann.raiho@gmail.com>
2018-11-01 16:00:46 -0400

    changing to pecan standard units for agb and adding gwbi. units in mg/ha/yr because kg/m2/s doesn't make sense for linkages or tree rings.

--- models/linkages/R/read_restart.LINKAGES.R
+++ models/linkages/R/read_restart.LINKAGES.R
@@ -29,7 +29,7 @@ read_restart.LINKAGES <- function(outdir, runid, stop.time, settings, var.names 
                      outdir = file.path(outdir, runid), 
                      start.year = lubridate::year(stop.time), 
                      end.year = lubridate::year(stop.time),
-                     variables = var.names,pft.name = unlist(sapply(settings$pfts,'[[', "name")))  # change to just 'AGB' for plot level biomass
+                     variables = var.names, pft.name = unlist(sapply(settings$pfts,'[[', "name")))  # change to just 'AGB' for plot level biomass
   if(!is.na(ens)){
   # Add PFT name to variable if applicable
   pft.names <- numeric(length(settings$pfts))
@@ -42,13 +42,18 @@ read_restart.LINKAGES <- function(outdir, runid, stop.time, settings, var.names 
   forecast <- list()
 
   if ("Fcomp" %in% var.names) {
-    forecast[[length(forecast)+1]] <- ens$AGB.pft #already has C  #* unit.conv 
+    forecast[[length(forecast)+1]] <- ens$AbvGrndWood #already has C  #* unit.conv 
     names(forecast[[length(forecast)]]) <- paste0('Fcomp.',pft.names)
   }
   
-  if ("AGB.pft" %in% var.names) {
-    forecast[[length(forecast)+1]] <- ens$AGB.pft #already has C  #* unit.conv 
-    names(forecast[[length(forecast)]]) <- paste0('AGB.pft.',pft.names)
+  if ("AbvGrndWood" %in% var.names) {
+    forecast[[length(forecast)+1]] <- ens$AbvGrndWood #already has C  #* unit.conv 
+    names(forecast[[length(forecast)]]) <- paste0('AbvGrndWood_',pft.names)
+  }
+  
+  if ("GWBI" %in% var.names) {
+    forecast[[length(forecast)+1]] <- udunits2::ud.convert(ens$GWBI,'kg/m^2/s','Mg/ha/yr') #udunits2::ud.convert(ens$TotSoilCarb, "kg/m^2", "Mg/ha") #* unit.conv 
+    names(forecast[[length(forecast)]]) <- paste0("GWBI_",pft.names)
   }
     
   if ("TotSoilCarb" %in% var.names) {
@@ -58,7 +63,10 @@ read_restart.LINKAGES <- function(outdir, runid, stop.time, settings, var.names 
   
   }else{
     forecast <- list()
-    if ("AGB.pft" %in% var.names) {
+    if ("AbvGrndWood" %in% var.names) {
+      forecast[[length(forecast)+1]] <- rep(NA,length(settings$pfts))
+    }
+    if ("GWBI" %in% var.names) {
       forecast[[length(forecast)+1]] <- rep(NA,length(settings$pfts))
     }
     if ("Fcomp" %in% var.names) {
