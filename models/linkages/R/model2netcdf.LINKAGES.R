e56258806ef41bf1794b06dd91838ae240847a99
araiho <ann.raiho@gmail.com>
2018-11-01 15:59:38 -0400

    adding overwrite and npp/gwbi by species

--- models/linkages/R/model2netcdf.LINKAGES.R
+++ models/linkages/R/model2netcdf.LINKAGES.R
@@ -18,12 +18,14 @@
 ##' @param sitelon Longitude of the site
 ##' @param start_date Start time of the simulation
 ##' @param end_date End time of the simulation
+##' @param overwrite if TRUE Overwrite files
 ##' @export
 ##'
 ##' @author Ann Raiho, Betsy Cowdery
 ##' @importFrom ncdf4 ncdim_def ncvar_def
 model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL,
-                                  end_date = NULL, pft_names = NULL) {
+                                  end_date = NULL, pft_names = NULL,
+                                  overwrite = FALSE) {
   # , PFTs) { logger.severe('NOT IMPLEMENTED')
   
   library(PEcAn.utils)
@@ -45,7 +47,7 @@ model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL,
   
   ### Loop over years in linkages output to create separate netCDF outputs
   for (y in seq_along(years)) {
-    if (file.exists(file.path(outdir, paste(years[y], "nc", sep = ".")))) {
+    if (file.exists(file.path(outdir, paste(years[y], "nc", sep = "."))) & overwrite == FALSE) {
       next
     }
     print(paste("---- Processing year: ", years[y]))  # turn on for debugging
@@ -64,16 +66,16 @@ model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL,
     output[[3]]  <- total.soil.carbon[y, ]  # TotSoilCarb in kgC/m2
     output[[4]]  <- c(ag.biomass[y, ], total.soil.carbon[y, ], leaf.litter[y, ], area[y, ])  # Carb Pools in kgC/m2
     output[[5]]  <- c("AGB", "Soil Organic Matter", "Leaf Litter", "LAI")  # poolname
-    output[[6]]  <- ag.npp[y, ]  # GWBI = NPP in linkages
+    output[[6]]  <- npp.spp.save[,y,]  # GWBI = NPP in linkages
     output[[7]]  <- hetero.resp[y, ]  # HeteroResp in kgC/m^2/s
-    output[[8]]  <- ag.npp[y, ]  # NPP = GWBI in linkages
+    output[[8]]  <- npp.spp.save[,y, ]  # NPP = GWBI in linkages
     output[[9]]  <- nee[y, ]  # NEE #possibly questionable
     output[[10]] <- et[y, ]  # Evap in kg/m^2/s
     output[[11]] <- agb.pft[, y, ]
     output[[12]] <- f.comp[, y]
     output[[13]] <- area[y, ]  #LAI
     output[[14]] <- water[y, ]  #soil moisture
-    output[[15]] <- abvgroundwood.biomass[y,] #AbvGroundWood just wood no leaves
+    output[[15]] <- agb.pft[, y, ] #AbvGroundWood just wood no leaves
     output[[16]] <- seq_along(pft_names) 
     
     # ******************** Declare netCDF variables ********************#
@@ -103,16 +105,16 @@ model2netcdf.LINKAGES <- function(outdir, sitelat, sitelon, start_date = NULL,
     var[[3]]  <- PEcAn.utils::to_ncvar("TotSoilCarb", dims)
     var[[4]]  <- ncdf4::ncvar_def("CarbPools", "kgC/m2", list(dim.cpools, dim.lat, dim.lon, dim.t), -999)
     var[[5]]  <- ncdf4::ncvar_def("poolnames", units = "", dim = list(dim.string, dim.cpools1), longname = "Carbon Pool Names", prec = "char")
-    var[[6]]  <- ncdf4::ncvar_def("GWBI", "kgC/m2", list(dim.lat, dim.lon, dim.t), -999)
+    var[[6]]  <- ncdf4::ncvar_def("GWBI", "kgC/m2", list(dim.lat, dim.lon, dim.t,dim.pfts), -999)
     var[[7]]  <- ncdf4::ncvar_def("HeteroResp", "kgC/m2/s", list(dim.lat, dim.lon, dim.t), -999)
-    var[[8]]  <- ncdf4::ncvar_def("NPP", "kgC/m2/s", list(dim.lat, dim.lon, dim.t), -999)
+    var[[8]]  <- ncdf4::ncvar_def("NPP", "kgC/m2/s", list(dim.lat, dim.lon, dim.t,dim.pfts), -999)
     var[[9]]  <- ncdf4::ncvar_def("NEE", "kgC/m2/s", list(dim.lat, dim.lon, dim.t), -999)
     var[[10]] <- ncdf4::ncvar_def("Evap", "kg/m2/s", list(dim.lat, dim.lon, dim.t), -999)
     var[[11]] <- ncdf4::ncvar_def("AGB.pft", "kgC/m2", list(dim.lat, dim.lon, dim.t, dim.pfts), -999)
     var[[12]] <- ncdf4::ncvar_def("Fcomp", "kgC/kgC", list(dim.lat, dim.lon, dim.t, dim.pfts), -999)
     var[[13]] <- ncdf4::ncvar_def("LAI", "m2/m2", list(dim.lat, dim.lon, dim.t), -999)
     var[[14]] <- ncdf4::ncvar_def("SoilMoist", "m2/m2", list(dim.lat, dim.lon, dim.t), -999)
-    var[[15]] <- ncdf4::ncvar_def("AbvGrndWood", "kgC/m2", list(dim.lat, dim.lon, dim.t), -999)
+    var[[15]] <- ncdf4::ncvar_def("AbvGrndWood", "kgC/m2", list(dim.lat, dim.lon, dim.t,dim.pfts), -999)
     var[[16]] <- ncdf4::ncvar_def("PFT", units = "", dim = list(dim.pfts),  
                      longname = paste(pft_names, collapse=","))
     
